9022dee6eedaabf06c30c727594c0ae2da9eae45
JaroslawX <ad.jaro@wp.pl>
2024-04-29 11:23:37 +0200

    Add first sites controllers, thank_you site, updates

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("all_elections/", views.all_elections, name="all_elections"),
-    path("vote/", views.vote, name="vote")
+    path("vote/<int:election_id>/", views.vote, name="vote"),
+    path("thank_you/", views.thank_you, name="thank_you"),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Election, Candidate, Vote, Report
 
 
 def home(request):
@@ -7,8 +8,22 @@ def home(request):
 
 
 def all_elections(request):
-    return render(request, "all_elections.html", {"all_elections": all_elections})
+    elections = Election.objects.all()  # We retrieve all selections from the database
+    return render(request, "all_elections.html", {"all_elections": elections})
 
 
-def vote(request):
-    return render(request, "vote.html")
+def vote(request, election_id):
+    if request.method == "POST":
+        candidate_id = request.POST.get("candidate")
+        candidate = Candidate.objects.get(pk=candidate_id)
+        candidate.votes += 1  # We increase the number of votes for the selected candidate
+        candidate.save()  # We save the changes in the database
+        return redirect("thank_you")  # Redirected to a thank you page
+    else:
+        election = Election.objects.get(pk=election_id)
+        candidates = Candidate.objects.filter(id_election=election_id)
+        return render(request, "vote.html", {"election": election, "candidates": candidates})
+
+
+def thank_you(request):
+    return render(request, "thank_you.html")
