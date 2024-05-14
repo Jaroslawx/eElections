85ce50c65711be43bc3b8732e642724b1c97ec29
JaroslawX <ad.jaro@wp.pl>
2024-05-14 10:46:08 +0200

    update urls and login

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,26 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import include, path, re_path
 from . import views
+
+from base.views import signup, login, logout
+from base.views import CustomPasswordResetView, CustomPasswordResetDoneView, CustomPasswordResetConfirmView, \
+    CustomPasswordResetCompleteView
+
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("all_elections/", views.all_elections, name="all_elections"),
     path("vote/<int:election_id>/", views.vote, name="vote"),
     path("thank_you/", views.thank_you, name="thank_you"),
-    # path("login/", views.login, name='login'),
-    # path("signup/", views.signup, name='signup'),
+
+    path("accounts/signup/", signup, name="signup"),
+    path("accounts/login/", login, name='login'),
+    path("accounts/logout/", logout, name='logout'),
+
+    path("accounts/password_reset/", CustomPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(),
+         name='custom_password_reset_confirm'),
+    path('accounts/reset/done', CustomPasswordResetCompleteView.as_view(), name='custom_password_reset_complete'),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
